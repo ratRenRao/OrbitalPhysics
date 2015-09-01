af96df074d4cd44f7b26717cc13f0f6da5514f19
Destructobot <brichardson@hotmail.com>
2015-09-01 16:36:04 -0600

    Initial Commit

---
+++ OrbitalPhysics/CelestialObject.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrbitalPhysics
+{
+    class CelestialObject
+    {
+        private float diameter, mass, velocity, xpos, ypos;
+
+        public CelestialObject(float diameter, float mass, float velocity, float xpos, float ypos)
+        {
+            this.diameter = diameter;
+            this.mass = mass;
+            this.velocity = velocity;
+            this.xpos = xpos;
+            this.ypos = ypos;
+        }
+
+        public float getDiameter() { return diameter; }
+        public float getMass() { return mass; }
+
+        public float getVelocity() { return velocity; }
+        public void setVelocity(float velocity) { this.velocity = velocity; }
+
+        public float getX() { return xpos; }
+        public void setX(float xpos) { this.xpos = xpos; }
+
+        public float getY() { return ypos; }
+        public void setY(float ypos) { this.ypos = ypos; }
+    }
+}
---
+++ OrbitalPhysics/Orbit.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Windows.Forms;
+
+namespace OrbitalPhysics
+{
+    class Orbit
+    {
+        public static Form form = new Form();
+        public static System.Drawing.Graphics graphicsObject = form.CreateGraphics();
+        public static List<CelestialObject> objectList = new List<CelestialObject>();
+
+        public void start()
+        {
+            form.BackColor.Equals(Color.Black);
+            form.Size.Equals(new Size(1000, 800));
+            form.Show();
+
+            objectList.Add(new CelestialObject(10, 10, 100, 50, 100));
+            objectList.Add(new CelestialObject(10, 10, 200, 305, 100));
+            foreach (CelestialObject celObject in objectList)
+            {
+                Paint(celObject.getDiameter(), celObject.getX(), celObject.getY());
+            }
+            System.Threading.Thread.Sleep(5000);
+        }
+
+        public void Paint(float diameter, float xpos, float ypos)
+        {
+            graphicsObject.FillEllipse(Brushes.Red, xpos, ypos, diameter, diameter);
+        }
+    }
+}
